7263ad50d9b041d120d0259c8fb16acc98057526
Colin MacArthur <colinpmacarthur@gmail.com>
2014-11-30 14:34:27 -0500

    Removing build shell script from the repository.

--- build.sh
+++
@@ -1,3 +0,0 @@
-export CLASSPATH=$CLASSPATH:/usr/share/java/saxonb.jar
-java net.sf.saxon.Transform -ext:on -o:portfolio.html -s:portfolio.xml -xsl:portfolio.xsl
-weasyprint resume.html macarthur_resume.pdf --stylesheet weasy.css
